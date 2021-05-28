79c8b827f853a3f658177191b564e487b6589ecc
Burak Öğretici <74312392+burakogreticii@users.noreply.github.com>
2021-05-28 11:05:25 +0300

    Initial commit

---
+++ src/test/java/kodlamaio/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,14 @@
+  
+package kodlamaio.northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/kodlamaio/northwind/SpringBootTest.java
@@ -0,0 +1,5 @@
+package kodlamaio.northwind;
+
+public @interface SpringBootTest {
+
+}
